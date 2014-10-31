6a801d0eec6ab1d76d8c14031dc41adad507045f
WhosTheMark <whosthemark@gmail.com>
2014-10-31 22:48:35 +0100

    Words from extra/stopliste.txt are not insterted in the database.

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,24 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.TreeSet;
 
 import controlleur.Indexation;
 
 
 public class Main {
 
-	public static void main(String[] args) {
-		File folder = new File("/home/marcos/workspace/search-engine/extra/CORPUS");
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		File folder = new File("extra/CORPUS");
 		File[] listOfFiles = folder.listFiles();
+		TreeSet<String> emptyWordsSet = Indexation.createEmptyWordsSet();
 		
 		System.out.println("Index process started.");
 		int i = 0;
 		
 		for (File file: listOfFiles) {
 			System.out.println("Indexing file: " + file.getName());
-			Indexation.createInverseFile(file, i++);
+			Indexation.createInverseFile(file, i++, emptyWordsSet);
 		}
 		
 		System.out.println("Index finished.");
--- src/controlleur/Indexation.java
+++ src/controlleur/Indexation.java
@@ -1,9 +1,11 @@
 package controlleur;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.HashMap;
-
+import java.util.Scanner;
+import java.util.TreeSet;
 import model.DBDriver;
 
 import org.jsoup.Jsoup;
@@ -16,31 +18,30 @@ public class Indexation {
 	private static final String separatorRegExpr = "[^A-Za-z0-9éàèùâêîôûëïüÿçœæ]+";
 	 
 	
-	public static void createInverseFile(File file, int document){
+	public static void createInverseFile(File file, int document, TreeSet<String> emptyWordsSet){
 		
 		DBDriver.storeDocument(document,file);
-		
 		Elements elems = parse(file);
-		
 		HashMap<String, Integer> inverseFile = new HashMap<String, Integer>();
 		
 		for (Element e : elems){
 			
 			String elemStr = e.ownText();
 			String[] words = elemStr.split(separatorRegExpr);
-			calculateFrequencies(words,inverseFile);
+			calculateFrequencies(words, inverseFile, emptyWordsSet);
 
 		}
 		
 		DBDriver.storeInverseFile(inverseFile, document);
-		
 	}
 	
-	private static void calculateFrequencies(String[] words, HashMap<String, Integer> inverseFile){
+	private static void calculateFrequencies(String[] words, 
+			HashMap<String, Integer> inverseFile, TreeSet<String> emptyWordsSet){
 		
 		for(String word: words) {
 			
-			if (!word.isEmpty()){
+			if (!word.isEmpty() && !emptyWordsSet.contains(word)){
+				
 				String lowerCaseWord = word.toLowerCase();
 				int frequency = 1;
 				
@@ -67,6 +68,32 @@ public class Indexation {
 		
 		Elements elems = doc.select("*");
 		return elems;		
+	}
+	
+	public static TreeSet<String> createEmptyWordsSet(){
+		
+		File file = new File("extra/stopliste.txt");
+		Scanner scanner;
+		
+		try {
+			scanner = new Scanner(file);
+		
+		} catch (FileNotFoundException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+			return null;
+		}
+		
+		scanner.useDelimiter(separatorRegExpr);
+		TreeSet<String> set = new TreeSet<String>();
+		
+		while (scanner.hasNext()){
+			String word = scanner.next();
+			set.add(word);
+		}
+		
+		return set;
+		
 	}
 
 }
