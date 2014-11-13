18f7f3fb9a33fe5cf974800edca938baeb1355c2
WhosTheMark <whosthemark@gmail.com>
2014-11-13 22:43:20 +0100

    Modified the DBDriver Better maintainability and added methods to retrieve data.

--- src/model/DBDriver.java
+++ src/model/DBDriver.java
@@ -5,18 +5,25 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map.Entry;
 
 public class DBDriver {
 
-	private static Connection conn = ConnectionBuilder.getConnection();
+	private static final Connection conn = ConnectionBuilder.getConnection();
+	private static final String wordTable = "word";
+	private static final String indexTable = "indx";
+	private static final String docTable = "document";
+	
 	
 	public static void storeDocument(int idDocument, File document){
 		
 		try {
 			
-			String strStmt = "INSERT INTO document VALUES (?,?);";
+			String strStmt = "INSERT INTO " + docTable + " VALUES (?,?);";
 			PreparedStatement prepstmt = conn.prepareStatement(strStmt);
 			prepstmt.setInt(1, idDocument);
 			prepstmt.setString(2,document.getName());
@@ -39,7 +46,7 @@ public class DBDriver {
 	
 	private static boolean wordExists(String word) throws SQLException{
 		
-		String strQuery = "SELECT * FROM word WHERE id_word=?;";
+		String strQuery = "SELECT * FROM " + wordTable + " WHERE id_word=?;";
 		PreparedStatement prepstmt = conn.prepareStatement(strQuery);
 		prepstmt.setString(1, word);
 		ResultSet rs = prepstmt.executeQuery();
@@ -53,7 +60,7 @@ public class DBDriver {
 			//If it does not exist then insert
 			if (!wordExists(word)) {
 				
-				String strStmt = "INSERT INTO word VALUES (?);";
+				String strStmt = "INSERT INTO " + wordTable + " VALUES (?);";
 				PreparedStatement prepstmt = conn.prepareStatement(strStmt);
 				prepstmt.setString(1, word);
 				prepstmt.executeUpdate();
@@ -61,14 +68,14 @@ public class DBDriver {
 			}
 			
 		} catch (SQLException e) {
-			System.out.println("ERROR: " + word);
+			System.out.println("ERROR storing the word: " + word);
 			System.out.println(e.getMessage());
 		}		
 	}
 	
 	private static void insertEntry(String word, int document, int weight) {
 		
-		String strStmt = "INSERT INTO indx VALUES (?,?,?);";
+		String strStmt = "INSERT INTO " + indexTable + " VALUES (?,?,?);";
 		
 		try {
 			
@@ -79,9 +86,93 @@ public class DBDriver {
 			prepstmt.execute();
 			
 		} catch (SQLException e) {
+			System.out.println("ERROR storing the index: " + word + " " + document);
 			System.out.println(e.getMessage());
 		}
 	}
+	
+	
+	/* 
+	 * Returns the list of frequencies of a word, the list is 
+	 * ordered in the same way documents are ordered in the
+	 * database.
+	 */
+	public static List<Integer> getFreqs(String word){
+		
+		String strQuery = "SELECT * FROM " + indexTable + 
+				" WHERE id_word=? ORDER BY document;";
+		PreparedStatement prepstmt;
+		
+		try {
+			prepstmt = conn.prepareStatement(strQuery);
+			prepstmt.setString(1, word);
+			ResultSet rs = prepstmt.executeQuery();
+			ArrayList<Integer> list = new ArrayList<Integer>();
+			
+			while(rs.next()){
+				list.add(rs.getInt("weight"));
+			}
+			
+			return list;
+			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+	public static ArrayList<RelevanceOfDoc> getDocuments(){
+		
+		String strQuery = "SELECT * FROM " + docTable + 
+				" WHERE ORDER BY id_document;";
+		Statement stmt;
+		
+		try {
+			stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery(strQuery);
+			ArrayList<RelevanceOfDoc> list = new ArrayList<RelevanceOfDoc>();
+			
+			while(rs.next()){
+				
+				int docId = rs.getInt("id_document");
+				String docName = rs.getString("name");
+				RelevanceOfDoc relevance = new RelevanceOfDoc(docId,docName);
+				list.add(relevance);
+			}
+			
+			return list;
+			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+		return null;
+	}
+	
+	public static void eraseDB(){
+		
+		String strDelete = "DELETE FROM ?";
+		
+		try {
+			PreparedStatement prepstmt = conn.prepareStatement(strDelete);
+			prepstmt.setString(1, indexTable);
+			prepstmt.executeUpdate();
+			prepstmt.setString(1, wordTable);
+			prepstmt.executeUpdate();
+			prepstmt.setString(1, docTable);
+			prepstmt.executeUpdate();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
 	
 	
 }
